b1fee5589ff04c3e6a281c4f775f27a59027fcdc
AkciumKicum <32580360+AkciumKicum@users.noreply.github.com>
2022-07-05 12:04:36 +0300

    edit sidebar minor

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -235,6 +235,7 @@ module.exports = {
       }]
       }, {
       title: 'Соглашения',
+      path: '/ext/agreements/',
       collapsable: false,
       children: [{
         title: 'Политика конфиденциальности',
